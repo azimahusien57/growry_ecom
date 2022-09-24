96b2c26965383536ed9d8d41a2203661ffec3922
azimahusien57 <azimahusien57@gmail.com>
2022-09-24 21:30:22 +0700

    first commit

---
+++ src/components/Category.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-item-group mandatory class="mt-n1">
+    <v-container>
+      <v-row justify="center" class="space">
+        <v-col
+          cols="12"
+          xs="12"
+          sm="4"
+          md="2"
+          v-for="(category, i) in categories" :key="i"
+        >
+          <v-item v-slot="{ active, toggle }">
+            <v-card :color="active ? '#D5F0DB' : 'white'" :class="active ? 'borderme':'borderout'"
+            class="d-flex align-center rounded-lg mx-2" dark height="180" flat @click="toggle">
+            <v-row>
+                <v-col cols="12" sm="12">
+                    <v-list-item three-line class="text-center">
+                        <v-list-item-content>
+                            <div align="center" justify="center">
+                                <v-img :src="categories.img" max-height="80" max-width="80" contain></v-img>
+                                
+                            </div>
+                            <v-list-item-subtitle
+                            :class="active ?'green--text' :'black--text'"
+                            class="caption mt-4">
+                            {{category.title}}
+
+                            </v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
+               </v-col>
+            </v-row>
+           
+            </v-card>
+          </v-item>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-item-group>
+</template>
+<!-- scrip js -->
+<script>
+export default {
+    data(){
+      return{
+        categories:[ 
+            {img: "../assets/categories/categories-baby.svg", title: "meet &fish"},
+            {img: "../assets/categories/categories-baby.svg", title: "meet &fish"},
+            {img: "../assets/categories/categories-baby.svg", title: "meet &fish"},
+            {img: "../assets/categories/categories-baby.svg", title: "meet &fish"},
+            {img: "../assets/categories/categories-baby.svg", title: "meet &fish"},
+            {img: "../assets/categories/categories-baby.svg", title: "meet &fish"},
+        ]
+    }
+    }
+   
+}
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app-bar color="white" flat>
+    <v-badge color="#D5F0DB" dot>
+      <v-img src="../assets/icon/icon-cart-empty.svg" class="icon_cart"></v-img>
+    </v-badge>
+    <v-toolbar-title class="ml=4">
+      <span class="green--text"></span><strong>RainBoz</strong>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-badge color="#FF6D59" overlap content="2" class="mr-2 mt-1">
+      <v-avatar color="#FFF0EE" size="40">
+        <v-icon x-small color="#FF6D59">fa fa-heart</v-icon>
+      </v-avatar>
+    </v-badge>
+    <v-badge color="#367E18" overlap content="2" class="mr-2 mt-1">
+      <v-avatar color="#ECF7EE" size="40">
+        <v-icon x-small color="#367E18">fas fa-shopping-cart</v-icon>
+      </v-avatar>
+    </v-badge>
+    <span class="grey--text d-none d-sm-flex"
+      >Hello,<strong>chika</strong></span
+    >
+    <v-avatar size="40" class="ml-2">
+      <v-img src="../assets/pp/icon-testimonial-3.png"></v-img>
+    </v-avatar>
+    <v-btn icon dark>
+      <v-icon x-small color="#878A94">fas fa-chevron-down</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+<!-- script js -->
+<script>
+// import { defineComponent } from '@vue/composition-api'
+
+export default {};
+</script>
+<!-- css -->
+<style>
+.v-toolbar_title {
+  font-size: 1rem !important;
+}
+</style>
